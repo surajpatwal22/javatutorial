1e878359465071cb1d0bd73b39198e7c545bbbbf
surajpatwal22 <patwal0suraj@gmail.com>
2023-04-13 23:43:03 +0530

    navBar

---
+++ navBartoggler/script.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector(".nav-toggle");
+const Links = document.querySelector(".links");
+
+navToggle.addEventListener('click',function () {
+    // console.log(Links.classList.contains("links"));
+    if (Links.classList.contains("show-links")) {
+        Links.classList.remove("show-links")
+    } else {
+        Links.classList.add("show-links")
+    }
+    // Links.classList.toggle("show-links");
+})
